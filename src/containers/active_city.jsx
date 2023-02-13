392e881149303656d400fbac8978bcf4752f3f53
Tom Kitson <thomaspfkitson@gmail.com>
2023-02-13 18:51:04 +0000

    complete

--- src/containers/active_city.jsx
+++ src/containers/active_city.jsx
@@ -3,14 +3,25 @@ import { connect } from 'react-redux';
 
 class ActiveCity extends Component {
   render() {
-    console.log(this.props.activeCity);
+    const exists = () => {
+      if (this.props.activeCity) {
+        return (
+          <div className="active-city">
+            <h1>{this.props.activeCity.name}</h1>
+            <p>{this.props.activeCity.address}</p>
+            <img src={this.props.activeCity.image} alt="" />
+          </div>
+        );
+      } else {
+        return <div className="active-city"></div>
+      }
+    };
     return (
-      <div className="active-city">
-        <h1>{this.props.activeCity ? this.props.activeCity.name : ''}</h1>
-      </div>
+      exists()
     );
   }
 }
+
 
 function mapStateToProps(reduxState) {
   return {
